5d8f4543922d35fcdec71cb3c564a532ce4efc38
aravindh <mail@aravindh.io>
2016-02-05 13:16:46 +0530

    Removed Vagrant

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-cookbook_file '/home/vagrant/queryapi.zip' do
+cookbook_file 'queryapi.zip' do
   source 'queryapi-0.2.0.zip'
   mode '0644'
 end
@@ -19,24 +19,24 @@ end
 # end
 package 'unzip'
 
-directory '/home/vagrant/queryapi' do
+directory 'queryapi' do
   mode '0644'
   recursive true
   action :delete
-  not_if { !::File.directory?(::File.join('/home/vagrant', 'queryapi')) }
+  not_if { !::File.directory?(::File.join('.', 'queryapi')) }
 end
 
 
 execute 'unzip' do
-  command 'unzip -o /home/vagrant/queryapi.zip -d /home/vagrant/queryapi'
+  command 'unzip -o ./queryapi.zip -d ./queryapi'
 end
 
-remote_file "/home/vagrant/queryapi/conf/config.hocon" do
-  source 'file:///home/vagrant/queryapi/conf/config.hocon.sample'
+remote_file "./queryapi/conf/config.hocon" do
+  source 'file://queryapi/conf/config.hocon.sample'
 end
 
 execute 'make run script executable' do
-  command 'chmod u+x /home/vagrant/queryapi/bin/run_queryapi.sh'
+  command 'chmod u+x ./queryapi/bin/run_queryapi.sh'
 end
 
 execute 'stop app' do
