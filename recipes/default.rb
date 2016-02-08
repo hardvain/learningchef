c75fddfd17dc3c96eb4a0bbf2cb86c37dd03af97
aravindh <mail@aravindh.io>
2016-02-08 13:09:41 +0530

    Changed log level

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+Chef::Log.level = :debug
 
 cookbook_file '/home/ec2-user/queryapi.zip' do
   source 'queryapi-0.2.0.zip'
