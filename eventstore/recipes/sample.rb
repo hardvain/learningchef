5b3ff6cd9a424028963e9c0dbceed7f1220ed208
aravindh <mail@aravindh.io>
2016-02-09 20:22:08 +0530

    /

--- eventstore/recipes/sample.rb
+++ eventstore/recipes/sample.rb
@@ -29,7 +29,9 @@ directory "#{node[:module][:path]}" do
   not_if { !::File.directory?(::File.join(node[:module][:path], node[:module][:name])) }
 end
 
-directory "#{node[:module][:path]}"
+directory "#{node[:module][:path]}" do
+  mode '0644'
+end
 
 remote_file "#{node[:module][:path]}.zip" do
   source "https://s3.amazonaws.com/espoc-apps/#{node[:module][:name]}.zip"
