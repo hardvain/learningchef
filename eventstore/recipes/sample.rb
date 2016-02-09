4cca33b2c437404c21657151089034a840a278bd
aravindh <mail@aravindh.io>
2016-02-09 18:52:40 +0530

    /

---
+++ eventstore/recipes/sample.rb
@@ -0,0 +1,55 @@
+#
+# Cookbook Name:: aws
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+module_name = "queryapi"
+user = "ec2-user"
+node.default['module']['name'] = module_name
+node.default['module']['path'] = "/home/#{user}/#{module_name}"
+node.default['module']['executable_path'] = "/home/#{user}/#{module_name}/bin/run_#{module_name}.sh"
+
+
+template "/etc/init.d/#{node[:module][:name]}" do
+  source 'init_script.erb'
+  mode '0755'
+  variables({
+     :module_name => "#{node[:module][:name]}",
+     :module_path => "#{node[:module][:path]}",
+     :module_executable => "#{node[:module][:executable_path]}"
+  })
+end
+
+package 'unzip'
+
+directory "#{node[:module][:path]}" do
+  mode '0644'
+  recursive true
+  action :delete
+  not_if { !::File.directory?(::File.join(node[:module][:path], node[:module][:name])) }
+end
+
+directory "#{node[:module][:path]}"
+
+remote_file "#{node[:module][:path]}.zip" do
+  source "https://s3.amazonaws.com/espoc-apps/#{node[:module][:name]}.zip"
+  action :create
+  mode '0644'
+end
+
+execute 'unzip' do
+  command "unzip -o #{node[:module][:path]}.zip -d #{node[:module][:path]}"
+end
+
+remote_file "#{node[:module][:path]}/conf/config.hocon" do
+  source "file://#{node[:module][:path]}/conf/config.hocon.sample"
+end
+
+execute 'make run script executable' do
+  user "root"
+  command "chmod u+x #{node[:module][:path]}/bin/run_#{node[:module][:name]}.sh"
+end
+
+execute 'start app' do
+  command "service #{node[:module][:name]} restart"
+end
