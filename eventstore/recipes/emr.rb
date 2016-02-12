1b4f81faa7c789f6142a4dfbeeebf225960818f5
aravindh <mail@aravindh.io>
2016-02-12 13:55:56 +0530

    Added shell script for managing emr

--- eventstore/recipes/emr.rb
+++ eventstore/recipes/emr.rb
@@ -1,4 +1,5 @@
 package 'unzip'
+package 'jq'
 
 directory "#{node[:module][:path]}" do
   mode '0644'
@@ -17,7 +18,7 @@ remote_file "#{node[:module][:path]}.zip" do
   mode '0644'
 end
 
-execute 'unzip' do
+execute 'unzip' do 
   command "unzip -o #{node[:module][:path]}.zip -d #{node[:module][:path]}"
 end
 
@@ -26,6 +27,15 @@ remote_file "#{node[:module][:path]}/conf/config.hocon" do
   source "file://#{node[:module][:path]}/conf/config.hocon.sample"
 end
 
+template "#{node[:module][:path]}/emr" do
+  source 'emr.erb'
+  mode '0755'
+  variables({
+     :cluster-id => "#{node[:module][:cluster-id]}",
+     :queue-url => "#{node[:module][:queue-url]}"
+  })
+end
+
 execute 'add step to emr' do
-  command "sh #{node[:module][:path]}/bin/run_#{node[:module][:name]}.sh"
+  command "sh #{node[:module][:path]}/bin/run_#{node[:module][:name]}.sh > #{node[:module][:path]}/bin/step_id"
 end
