dcbb85bb56ee679389abe5ef7bb394bbaf551b7b
aravindh <mail@aravindh.io>
2016-02-17 14:45:38 +0530

    .

--- eventstore/attributes/default.rb
+++ eventstore/attributes/default.rb
@@ -1,1 +1,2 @@
 default[:sample] = "Sample in default"
+default[:sample1] = "Sample1 in default"
--- eventstore/recipes/test.rb
+++ eventstore/recipes/test.rb
@@ -2,3 +2,8 @@ file '/home/ec2-user/message' do
   content node[:sample]
   mode '0755'
 end
+
+file '/home/ec2-user/message1' do
+  content node[:sample1]
+  mode '0755'
+end
