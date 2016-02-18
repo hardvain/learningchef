4ca944e7c880f84df606274f90cd99ba0472f259
aravindh <mail@aravindh.io>
2016-02-18 12:47:46 +0530

    .

--- eventstore/attributes/collector.rb
+++ eventstore/attributes/collector.rb
@@ -1,5 +1,5 @@
 node.default[:collector][:variables]= {
 	:aws_region => node.default[:aws_region],
-	:bad_steam_name => "espoc-bad-events",
+	:bad_stream_name => "espoc-bad-events",
 	:good_stream_name => "espoc-raw-events"
 }
