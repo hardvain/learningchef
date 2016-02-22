191f5dbcff2e2654b99c335b30a259ccfa3b645b
aravindh <mail@aravindh.io>
2016-02-22 14:07:24 +0530

    .

--- eventstore/recipes/ruby_setup.rb
+++ eventstore/recipes/ruby_setup.rb
@@ -2,7 +2,7 @@ execute 'install keyserver' do
     command "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
 end
 execute 'install rvm' do
-    command "\curl -sSL https://get.rvm.io | bash -s stable"
+    command " curl -sSL https://get.rvm.io | bash -s stable --ruby"
 end
 execute 'add rvm to profile' do
     command "source /etc/profile.d/rvm.sh"
